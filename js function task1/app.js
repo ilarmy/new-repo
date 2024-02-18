774c2bbd6cfba5b8a4a2e79bcf9b546512d8201f
ilarmy <151742698+ilarmy@users.noreply.github.com>
2024-02-18 22:06:02 +0400

    Delete js function task1 directory

--- js function task1/app.js
+++
@@ -1,9 +0,0 @@
-let userName = prompt("enter the name");
-function showMessage() {
-  let message = userName+" is active now";
-  console.log(message);
-}
-
-showMessage();
-showMessage();
-showMessage();
