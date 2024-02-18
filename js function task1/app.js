2b65b9d2319f2dc8125bce2f6a212c9195d1c71c
ilarmy <151742698+ilarmy@users.noreply.github.com>
2024-02-18 22:01:05 +0400

    Add files via upload

---
+++ js function task1/app.js
@@ -0,0 +1,9 @@
+let userName = prompt("enter the name");
+function showMessage() {
+  let message = userName+" is active now";
+  console.log(message);
+}
+
+showMessage();
+showMessage();
+showMessage();
