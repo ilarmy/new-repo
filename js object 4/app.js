891594430f79f9312cf0a30b56b0799b5b713596
ilarmy <151742698+ilarmy@users.noreply.github.com>
2024-02-21 16:30:30 +0400

    Add files via upload

---
+++ js object 4/app.js
@@ -0,0 +1,24 @@
+// let calculator= {
+//     number:0,
+//     toplama:function(num){
+//         this.number+=this.esas;
+//     },
+//     cixma:function (num) {
+//         this.number -= esas;
+//     },
+//     vurma:function(num){
+//         this.number*= this.esas;
+//     },
+//     bolme:function(num){
+//         if(this.esas!==0){
+//             this.number/=this.esas;
+//         }else{
+//             console.log('sifira bolme yoxdur')
+//         }
+//     }
+// };
+// calculator.toplama(5);
+// calculator.cixma(2);
+// calculator.vurma(3);
+// calculator.bolme(2);
+// console.log('cavab'+calculator.number);
