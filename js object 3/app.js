bcb812d495f3ae110fcfea72a0f97a94b04a1267
ilarmy <151742698+ilarmy@users.noreply.github.com>
2024-02-21 15:27:06 +0400

    Add files via upload

---
+++ js object 3/app.js
@@ -0,0 +1,16 @@
+// let user = {
+//     name: "Adınız",
+//     gender: "Cinsiniz",
+//     birthday: "Doğum Tarihiniz",
+//     Age:67,
+// };
+
+
+// user.getYearsBeforeRetirement = function () {
+//     if (this.age) {
+//         let yearsLeft = this.Age - this.age;
+//         return yearsLeft > 0 ? yearsLeft : "qocaldiz";
+//     } else {
+//         return "Yaşınızı qeyd edin.";
+//     }
+// };
